f7b8e037a41c973164af1b3f0d4af7744b5f52b0
Ben Hall <benji2357@gmail.com>
2018-01-01 14:07:12 +0000

    feat(api): Initial work on api

---
+++ api/src/database.js
@@ -0,0 +1,46 @@
+const mysql = require('mysql')
+
+const {
+  MYSQL_DATABASE,
+  MYSQL_HOST,
+  MYSQL_PASSWORD,
+  MYSQL_USER,
+} = process.env
+
+const appsQuery = `
+SELECT
+  apps.id,
+  apps.color,
+  apps.icon_url AS iconUrl,
+  apps.name,
+  apps.url,
+  GROUP_CONCAT(tags.id) AS tag_ids
+FROM apps
+JOIN app_tags ON apps.id = app_tags.app_id
+JOIN tags ON app_tags.tag_id = tags.id
+GROUP BY id
+`
+
+const tagsQuery = 'SELECT * FROM tags'
+
+const runQuery = query => new Promise((resolve, reject) => {
+  const connection = mysql.createConnection({
+    host: MYSQL_HOST,
+    user: MYSQL_USER,
+    password: MYSQL_PASSWORD,
+    database: MYSQL_DATABASE,
+  })
+
+  connection.connect()
+
+  connection.query(query, (error, results) => {
+    if (error) reject(error)
+    else resolve(results)
+  })
+
+  connection.end()
+})
+
+module.exports.getApps = () => runQuery(appsQuery)
+
+module.exports.getTags = () => runQuery(tagsQuery)
---
+++ api/src/index.js
@@ -0,0 +1,29 @@
+const cors = require('cors')
+const express = require('express')
+const {getApps, getTags} = require('./database')
+
+const {PORT} = process.env
+
+const app = express()
+
+app.use(cors())
+
+app.get('/apps', (req, res) => {
+  getApps()
+    .then(data => res.send(data))
+    .catch(err => {
+      res.status(500)
+      res.send(err)
+    })
+})
+
+app.get('/tags', (req, res) => {
+  getTags()
+    .then(data => res.send(data))
+    .catch(err => {
+      res.status(500)
+      res.send(err)
+    })
+})
+
+app.listen(PORT, () => process.stdout.write(`api listening on port ${PORT}\n`))
