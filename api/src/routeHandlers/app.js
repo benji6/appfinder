7497a53b273216cc52504170986c76007c3d8135
Ben Hall <benji2357@gmail.com>
2018-02-22 21:16:27 +0000

    refactor(api): Factor out database/app module

--- api/src/routeHandlers/app.js
+++ api/src/routeHandlers/app.js
@@ -1,8 +1,29 @@
 const pino = require('../pino')
-const {getApp} = require('../database/app')
+const runQuery = require('../database/runQuery')
+
+const query = `
+SELECT
+apps.id,
+apps.color,
+apps.description,
+apps.icon_url AS iconUrl,
+apps.name,
+AVG(reviews.rating) AS rating,
+apps.url,
+GROUP_CONCAT(DISTINCT categories.id) AS categoryIds
+FROM apps
+LEFT JOIN app_categories ON apps.id = app_categories.app_id
+LEFT JOIN categories ON app_categories.category_id = categories.id
+LEFT JOIN reviews ON apps.id = reviews.app_id
+WHERE apps.id = ?
+`
 
 exports.get = (req, res) => {
-  getApp(req.params.id)
+  runQuery(query, [Number(req.params.id)])
+    .then(([result]) => {
+      result.categoryIds = JSON.parse(`[${result.categoryIds}]`) // eslint-disable-line no-param-reassign
+      return result
+    })
     .then(data => res.send(data))
     .catch(err => {
       pino.error(err)
